8a0791189f0fa9962a2ba4dc1d07c28f5d219e39
mrsushil67 <mrsushil67@gmail.com>
2025-04-08 16:54:18 +0530

    use Globle Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { UserProvider } from "./globle/context.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <UserProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </UserProvider>
+);
