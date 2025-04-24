ff03c43ea8277e6b0f021dad725ea9b054bd4811
mrsushil67 <mrsushil67@gmail.com>
2025-04-24 14:38:58 +0530

    update height width od main page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -29,7 +29,7 @@ const Layout = () => {
 
   return (
     <div className="layout-container flex flex-col">
-      <div className="navbar bg-gray-100 text-black flex justify-between items-center p-4 fixed top-0 z-50 border-b-1">
+      <div className="navbar bg-gray-100 text-black flex justify-between items-center p-3 fixed top-0 z-50 border-b-1">
         <div className="flex text-gray-700 items-center gap-4">
           <button onClick={() => setSidebarOpen(!isSidebarOpen)}>
             {isSidebarOpen ? <IoClose size={25} /> : <IoMenu size={25} />}
@@ -67,7 +67,7 @@ const Layout = () => {
         </div>
       </div>
 
-      <div className="content-container flex flex-grow mt-14">
+      <div className="content-container flex flex-grow pt-10 md:pt-13 lg:pt-16">
         <div
           className={`sidebar bg-gray-100 text-black flex flex-col p-2 transition-all duration-400 ${
             isSidebarOpen ? "sidebar-open" : "sidebar-closed"
