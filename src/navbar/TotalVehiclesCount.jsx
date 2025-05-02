2fbf45ea3b33506cb8393de348c2acded049048e
mrsushil67 <mrsushil67@gmail.com>
2025-05-02 15:41:25 +0530

    added running and idle count in navbar

--- src/navbar/TotalVehiclesCount.jsx
+++ src/navbar/TotalVehiclesCount.jsx
@@ -17,6 +17,7 @@ const TotalVehiclesCount = () => {
     status: key,
     count: value,
   }));
+
   const handlePopoverOpen = (event) => {
     setHoverVehicleCount(event.currentTarget);
   };
@@ -27,26 +28,41 @@ const TotalVehiclesCount = () => {
   const id2 = openVehicleStatusFilter ? "mouse-over-popover" : undefined;
 
   return (
-    <div>
-      {totalVehicles > 0 && (
-        <div
-          ref={ref}
-          aria-owns={id2}
-          aria-haspopup="true"
-          onMouseEnter={handlePopoverOpen}
-          onMouseLeave={handlePopoverClose}
-        >
-          <span className="font-bold  text-gray-700">Total Vehicles : </span>
-          <span className="font-bold  text-[#fc6a2a]">{totalVehicles} </span>
-          <VehicleStatus
-            vehicleStatus={vehicleStatus}
-            hoverVehicleCount={hoverVehicleCount}
-            setHoverVehicleCount={setHoverVehicleCount}
-            openVehicleStatusFilter={openVehicleStatusFilter}
-            id2={id2}
-          />
-        </div>
-      )}
+    <div
+      className="flex"
+      ref={ref}
+      aria-owns={id2}
+      aria-haspopup="true"
+      onMouseEnter={handlePopoverOpen}
+      onMouseLeave={handlePopoverClose}
+    >
+      <div className="pr-4">
+        <span className="font-bold text-sm text-gray-700">Running : </span>
+        <span className="font-bold text-sm text-green-700">{count.RUNNING}</span>
+      </div>
+      <div className="pr-4">
+        <span className="font-bold text-sm text-gray-700">Idle : </span>
+        <span className="font-bold text-sm text-yellow-500">{count.IDLE}</span>
+      </div>
+      <div className="pr-4">
+        {totalVehicles > 0 && (
+          <div>
+            <span className="font-bold text-sm text-gray-700">
+              Total Vehicles :{" "}
+            </span>
+            <span className="font-bold text-sm text-[#fc6a2a]">
+              {totalVehicles}{" "}
+            </span>
+            <VehicleStatus
+              vehicleStatus={vehicleStatus}
+              hoverVehicleCount={hoverVehicleCount}
+              setHoverVehicleCount={setHoverVehicleCount}
+              openVehicleStatusFilter={openVehicleStatusFilter}
+              id2={id2}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
