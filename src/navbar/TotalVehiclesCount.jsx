d4e1a199ddd3256a7c1d29047d3096645edeb100
mrsushil67 <mrsushil67@gmail.com>
2025-05-02 16:33:57 +0530

    add spaces

--- src/navbar/TotalVehiclesCount.jsx
+++ src/navbar/TotalVehiclesCount.jsx
@@ -36,21 +36,21 @@ const TotalVehiclesCount = () => {
       onMouseEnter={handlePopoverOpen}
       onMouseLeave={handlePopoverClose}
     >
-      <div className="pr-4">
-        <span className="font-bold text-sm text-gray-700">Running : </span>
-        <span className="font-bold text-sm text-green-700">{count.RUNNING}</span>
+      <div className="pr-10">
+        <span className="font-bold text-gray-700">Running : </span>
+        <span className="font-bold text-green-700">{count.RUNNING}</span>
       </div>
-      <div className="pr-4">
-        <span className="font-bold text-sm text-gray-700">Idle : </span>
-        <span className="font-bold text-sm text-yellow-500">{count.IDLE}</span>
+      <div className="pr-10">
+        <span className="font-bold text-gray-700">Idle : </span>
+        <span className="font-bold text-yellow-500">{count.IDLE}</span>
       </div>
-      <div className="pr-4">
+      <div className="pr-10">
         {totalVehicles > 0 && (
           <div>
-            <span className="font-bold text-sm text-gray-700">
+            <span className="font-bold text-gray-700">
               Total Vehicles :{" "}
             </span>
-            <span className="font-bold text-sm text-[#fc6a2a]">
+            <span className="font-bold text-[#fc6a2a]">
               {totalVehicles}{" "}
             </span>
             <VehicleStatus
